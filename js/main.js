9886b1c61fc07c670a16ff0a72cd2e95017938e7
thaegglund <tommy.haegglund@implantdirect.com>
2016-06-15 16:58:01 -0700

    Fix + svg play

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['1st', '2nd', '3rd', '4th'],
-        sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#7baabe'],
+        sectionsColor: ['#182733', '#1BBC9B', '#7E8F7C', '#7baabe'],
         menu: '#navbar'
     });
 });
