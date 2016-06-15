b5b687d929d7de74ad5f3e7bde8cf133e07e0441
thaegglund <tommy.haegglund@implantdirect.com>
2016-06-15 10:18:03 -0700

    Fullpage & Boostrap - Template setup

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['1st', '2nd', '3rd', '4th'],
+        sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#7baabe'],
+        menu: '#navbar'
+    });
+});
